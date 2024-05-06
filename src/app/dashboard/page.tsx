d8abd5305086281bb117d392b02a9b7771ab58f8
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2024-05-05 19:26:54 -0500

    created basic dashboard page with list of current decks user made

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,33 +3,111 @@ import Navbar from '@/components/Navbar'
 
 const Dashboard: React.FC = () => {
   return (
-    <div className="flex min-w-screen min-h-screen">
-        {/* Navbar when logged in */}
-        <div className="fixed flex-col flex h-screen w-[300px] border-r-[0.5px] shadow-2xl pl-10 pt-24">
-            <div className="mb-24">
-                <h3 className="text-[23px]">Hi, Alex!</h3>
-            </div>
-
-            {/* Account Options */}
-            <div className="flex flex-col gap-10">
-                {/* Account security (change password, account info) */}
-                <h4>Account Settings</h4>
-                {/* Your Decks */}
-                <h4>View Your Decks</h4>
-                {/* Create a new Deck */}
-                <h4>Create a new deck</h4>
-            </div>
-            {/* Logout */}
-            <div className="flex flex-col h-full justify-end mb-14">
-                <h4 className="text-[18px] underline">Logout</h4>
-            </div>
-        </div>
-
-        {/* Rest of the page */}
-        <div>
+    <div className="flex flex-col min-w-screen min-h-screen items-center">
+        {/* Navbar on dash page */}
+        <div className="flex flex-col justify-center items-center w-[80%] h-[100px] my-20 ">
+            <h1 className="text-[30px] h-full">Welcome, Alex!<span className="text-[10px] bg-blue-300">settings</span></h1>
+            <h5 className="mt-3 underline">Logout</h5>
             
+            {/* nav options */}
+            <div className="flex items-center w-full justify-center mt-10">
+                <button className="bg-purple-200 border-[1px] border-purple-700 px-10 py-2 rounded-lg">+ Create New Deck</button>
+            </div>
         </div>
-        
+
+        {/* list of available decks */}
+        <div className="flex flex-col w-[100%] h-[100%] items-center gap-14">
+            {/* One deck and info */}
+            <div className="flex w-[600px] py-8 border-[1px] rounded-lg border-gray-500 justify-between px-8 shadow-lg">
+                <div className="flex flex-col gap-6 self-end">
+                    <h3 className="text-[22px]">Deck Title</h3>
+                    <p>deck description</p>
+                </div>
+                <div className="flex flex-col gap-6 self-end">
+                    <p>-------jlpt kanji level bar-------</p>
+                    <p className="self-end">modified date</p>
+                </div>
+                <div className="flex flex-col gap-6 self-end">
+                    <p className="text-red-600 self-end">delete</p>
+                    <p className="text-blue-600 self-end">edit</p>
+                </div>
+            </div>
+
+            {/* Duplicates just for current visual. need to put above code in separate component */}
+            {/* delete the below divs later */}
+            <div className="flex w-[600px] py-8 border-[1px] rounded-lg border-gray-500 justify-between px-8 shadow-lg">
+                <div className="flex flex-col gap-6 self-end">
+                    <h3 className="text-[22px]">Deck Title</h3>
+                    <p>deck description</p>
+                </div>
+                <div className="flex flex-col gap-6 self-end">
+                    <p>-------jlpt kanji level bar-------</p>
+                    <p className="self-end">modified date</p>
+                </div>
+                <div className="flex flex-col gap-6 self-end">
+                    <p className="text-red-600 self-end">delete</p>
+                    <p className="text-blue-600 self-end">edit</p>
+                </div>
+            </div>
+            <div className="flex w-[600px] py-8 border-[1px] rounded-lg border-gray-500 justify-between px-8 shadow-lg">
+                <div className="flex flex-col gap-6 self-end">
+                    <h3 className="text-[22px]">Deck Title</h3>
+                    <p>deck description</p>
+                </div>
+                <div className="flex flex-col gap-6 self-end">
+                    <p>-------jlpt kanji level bar-------</p>
+                    <p className="self-end">modified date</p>
+                </div>
+                <div className="flex flex-col gap-6 self-end">
+                    <p className="text-red-600 self-end">delete</p>
+                    <p className="text-blue-600 self-end">edit</p>
+                </div>
+            </div>
+            <div className="flex w-[600px] py-8 border-[1px] rounded-lg border-gray-500 justify-between px-8 shadow-lg">
+                <div className="flex flex-col gap-6 self-end">
+                    <h3 className="text-[22px]">Deck Title</h3>
+                    <p>deck description</p>
+                </div>
+                <div className="flex flex-col gap-6 self-end">
+                    <p>-------jlpt kanji level bar-------</p>
+                    <p className="self-end">modified date</p>
+                </div>
+                <div className="flex flex-col gap-6 self-end">
+                    <p className="text-red-600 self-end">delete</p>
+                    <p className="text-blue-600 self-end">edit</p>
+                </div>
+            </div>
+            <div className="flex w-[600px] py-8 border-[1px] rounded-lg border-gray-500 justify-between px-8 shadow-lg">
+                <div className="flex flex-col gap-6 self-end">
+                    <h3 className="text-[22px]">Deck Title</h3>
+                    <p>deck description</p>
+                </div>
+                <div className="flex flex-col gap-6 self-end">
+                    <p>-------jlpt kanji level bar-------</p>
+                    <p className="self-end">modified date</p>
+                </div>
+                <div className="flex flex-col gap-6 self-end">
+                    <p className="text-red-600 self-end">delete</p>
+                    <p className="text-blue-600 self-end">edit</p>
+                </div>
+            </div>
+            <div className="flex w-[600px] py-8 border-[1px] rounded-lg border-gray-500 justify-between px-8 shadow-lg">
+                <div className="flex flex-col gap-6 self-end">
+                    <h3 className="text-[22px]">Deck Title</h3>
+                    <p>deck description</p>
+                </div>
+                <div className="flex flex-col gap-6 self-end">
+                    <p>-------jlpt kanji level bar-------</p>
+                    <p className="self-end">modified date</p>
+                </div>
+                <div className="flex flex-col gap-6 self-end">
+                    <p className="text-red-600 self-end">delete</p>
+                    <p className="text-blue-600 self-end">edit</p>
+                </div>
+            </div>
+
+
+        </div>
     </div>
   )
 }
